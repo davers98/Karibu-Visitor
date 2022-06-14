3052df5666198c1dd693434746dc4d758841a231
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2022-06-14 23:13:15 +0300

    add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :visitor_logs
   root 'public#home'
   devise_for :users
+
+  get '/users', to: 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
