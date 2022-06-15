8fc3d1c60310e53039dfb6a04403cf1d53b1a621
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2022-06-15 11:24:35 +0300

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get '/users', to: 'users#index'
+  get '/user/:id', to: 'users#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
