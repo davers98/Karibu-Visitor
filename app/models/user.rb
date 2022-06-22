1bc02c2c7842b482013a1caa4b959ce5b6cddded
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2022-06-22 07:22:21 +0300

    remove fields from users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,4 @@ class User < ApplicationRecord
 
   has_many :user_logs
 
-  has_one_attached :image
-
 end
---
+++ db/migrate/20220622042136_remove_fields_from_users.rb
@@ -0,0 +1,7 @@
+class RemoveFieldsFromUsers < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :users, :name, :string
+    remove_column :users, :phone_number, :string
+    remove_column :users, :position, :string
+  end
+end
