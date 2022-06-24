d19731d3c404e896b421acea5bf1c6340177d11d
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2022-06-24 10:42:15 +0300

    Edit users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :position, :phone_number, :image)
+    params.require(:user).permit(:name, :position, :phone_number, :image, :password)
   end
 end
